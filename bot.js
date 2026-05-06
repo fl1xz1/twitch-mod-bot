922948f085ef5582ddb03dacae93cc645b1bf46a
fl1xz1 <Monster.gsrp@gmail.com>
2026-05-06 01:38:12 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+{
+  "name": "twitch-mod-bot",
+  "version": "1.0.0",
+  "description": "Discord bot that syncs Twitch moderators and assigns Discord roles",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1",
+    "axios": "^1.6.7"
+  }
+}
